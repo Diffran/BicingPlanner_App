29a432ea6cf7867feb64a04923477a6e4d72503f
Diana <dianafrancitorraferran@gmail.com>
2025-03-04 12:29:45 +0100

    wip

--- app/src/main/java/org/diffran/bicingplanner/MainActivity.kt
+++ app/src/main/java/org/diffran/bicingplanner/MainActivity.kt
@@ -37,7 +37,6 @@ fun MapViewCompose() {
                 onCreate(null)
                 getMapAsync { map ->
                     map.setStyle("https://maps.geo.us-east-2.amazonaws.com/maps/v0/maps/OpenDataStyle/style-descriptor?key=v1.public.eyJqdGkiOiI1NjY5ZTU4My0yNWQwLTQ5MjctODhkMS03OGUxOTY4Y2RhMzgifR_7GLT66TNRXhZJ4KyJ-GK1TPYD9DaWuc5o6YyVmlikVwMaLvEs_iqkCIydspe_vjmgUVsIQstkGoInXV_nd5CcmqRMMa-_wb66SxDdbeRDvmmkpy2Ow_LX9GJDgL2bbiCws0wupJPFDwWCWFLwpK9ICmzGvNcrPbX5uczOQL0N8V9iUvziA52a1WWkZucIf6MUViFRf3XoFkyAT15Ll0NDypAzY63Bnj8_zS8bOaCvJaQqcXM9lrbTusy8Ftq8cEbbK5aMFapXRjug7qcrzUiQ5sr0g23qdMvnKJQFfo7JuQn8vwAksxrQm6A0ByceEXSfyaBoVpFcTzEclxUomhY.NjAyMWJkZWUtMGMyOS00NmRkLThjZTMtODEyOTkzZTUyMTBi") {
-                        // Configurar la cámara para acercarse y mostrar calles
                         map.cameraPosition = CameraPosition.Builder()
                             .target(LatLng(41.3784, 2.1815))
                             .zoom(12.4)
