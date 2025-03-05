e0de7757da8867e709965fa20c4f241c5879a040
Diana <dianafrancitorraferran@gmail.com>
2025-03-05 14:05:16 +0100

    wip

--- app/src/main/java/org/diffran/bicingplanner/MainActivity.kt
+++ app/src/main/java/org/diffran/bicingplanner/MainActivity.kt
@@ -4,11 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import org.diffran.bicingplanner.ui.component.Pin
 import org.diffran.bicingplanner.ui.theme.BicingPlannerTheme
@@ -19,9 +31,14 @@ import org.maplibre.android.geometry.LatLng
 import org.maplibre.android.maps.MapLibreMap
 import org.maplibre.android.maps.MapView
 import org.maplibre.android.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions;
+import com.mapbox.mapboxsdk.plugins.annotation.Symbol;
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import org.maplibre.android.style.sources.GeoJsonOptions
 
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -37,33 +54,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MapViewCompose() {
-    Column {
-        Pin(painterResource(R.drawable.ic_quest_bicycle))
+    var symbolManager :SymbolManager
+    var symbol : Symbol
 
+    Column {
+        Row{
+            Pin(painterResource(R.drawable.bicing_logo_docks))
+            Pin(painterResource(R.drawable.bicing_logo_mecanica))
+            Pin(painterResource(R.drawable.bicing_logo_electrica))
+
+        }
         AndroidView(
             factory = { context ->
                 MapView(context).apply {
                     onCreate(null)
                     getMapAsync { map ->
-                        map.setStyle("https://maps.geo.us-east-2.amazonaws.com/maps/v0/maps/OpenDataStyle/style-descriptor?key=v1.public.eyJqdGkiOiI1NjY5ZTU4My0yNWQwLTQ5MjctODhkMS03OGUxOTY4Y2RhMzgifR_7GLT66TNRXhZJ4KyJ-GK1TPYD9DaWuc5o6YyVmlikVwMaLvEs_iqkCIydspe_vjmgUVsIQstkGoInXV_nd5CcmqRMMa-_wb66SxDdbeRDvmmkpy2Ow_LX9GJDgL2bbiCws0wupJPFDwWCWFLwpK9ICmzGvNcrPbX5uczOQL0N8V9iUvziA52a1WWkZucIf6MUViFRf3XoFkyAT15Ll0NDypAzY63Bnj8_zS8bOaCvJaQqcXM9lrbTusy8Ftq8cEbbK5aMFapXRjug7qcrzUiQ5sr0g23qdMvnKJQFfo7JuQn8vwAksxrQm6A0ByceEXSfyaBoVpFcTzEclxUomhY.NjAyMWJkZWUtMGMyOS00NmRkLThjZTMtODEyOTkzZTUyMTBi") {
+                            map.setStyle("https://maps.geo.us-east-2.amazonaws.com/maps/v0/maps/OpenDataStyle/style-descriptor?key=v1.public.eyJqdGkiOiI1NjY5ZTU4My0yNWQwLTQ5MjctODhkMS03OGUxOTY4Y2RhMzgifR_7GLT66TNRXhZJ4KyJ-GK1TPYD9DaWuc5o6YyVmlikVwMaLvEs_iqkCIydspe_vjmgUVsIQstkGoInXV_nd5CcmqRMMa-_wb66SxDdbeRDvmmkpy2Ow_LX9GJDgL2bbiCws0wupJPFDwWCWFLwpK9ICmzGvNcrPbX5uczOQL0N8V9iUvziA52a1WWkZucIf6MUViFRf3XoFkyAT15Ll0NDypAzY63Bnj8_zS8bOaCvJaQqcXM9lrbTusy8Ftq8cEbbK5aMFapXRjug7qcrzUiQ5sr0g23qdMvnKJQFfo7JuQn8vwAksxrQm6A0ByceEXSfyaBoVpFcTzEclxUomhY.NjAyMWJkZWUtMGMyOS00NmRkLThjZTMtODEyOTkzZTUyMTBi") {
                             map.cameraPosition = CameraPosition.Builder()
                                 .target(LatLng(41.3784, 2.1815))
                                 .zoom(12.4)
                                 .build()
-                            map.setMinZoomPreference(11.0)
-                            map.setMaxZoomPreference(18.0)
+                           // map.setMinZoomPreference(11.0)
+                           // map.setMaxZoomPreference(18.0)
+                                var geoJsonOptions : GeoJsonOptions =
                         }
-                        addMarker(map)
+                        //map.addMarker() -> deprecated, si no ho aconsegueixo mirar d'anar per aquí
                     }
                 }
             },
             modifier = Modifier.fillMaxSize()
         )
     }
-}
-
-fun addMarker(map : MapLibreMap){
-
 }
 
 
