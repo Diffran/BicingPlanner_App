0c6167e45bd4a592d6475357344f08cfb38761ce
Diana <dianafrancitorraferran@gmail.com>
2025-04-02 13:56:32 +0200

    wip

--- app/src/main/java/org/diffran/bicingplanner/data/RetrofitService.kt
+++ app/src/main/java/org/diffran/bicingplanner/data/RetrofitService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RetrofitService {
-    @GET("bicing_data")
+    @GET("bicing_geojson")
     suspend fun getGeoJsonFromApi(@Query("type") type: String,
                                   @Query("time") time: Int) : ResponseBody
 }
